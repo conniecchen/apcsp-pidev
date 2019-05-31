9c2b9e677597130246d74a25ad7580cec3f28c2e
conniecchen <33041357+conniecchen@users.noreply.github.com>
2019-05-31 09:17:22 -0700

    Update areaofcircle2.c

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,34 +1,64 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <math.h>
 
-float areaofCircle(float radius) {
-  float area = radius*radius*M_PI;
-  return area;
+
+float myAreaFunction(float radius)
+{
+	float area = M_PI * radius * radius;
+	return area;
 }
 
-int main(int argc, char* argv[]) {
-  char inString[256];
-  float minR, maxR, temp;
-  if (argc < 3) {
-    printf("Please input the minimum value:\n");
-    fgets(inString, 256, stdin);
-    sscanf(inString, "%f", &minR);
-    printf("Please input the maximum value:\n");
-    fgets(inString, 256, stdin);
-    sscanf(inString, "%f", &maxR);
-  }
-  else {
-    sscanf(argv[1], "%f", &minR);
-    sscanf(argv[2], "%f", &maxR);
-  }
-  if (minR > maxR) {
-      printf("Minimum value is greater than the maximum value, using minimum as maximum\n");
-      temp = minR;
-      minR = maxR;
-      maxR = temp;
-    } 
-  for (float i = minR; i <= maxR; i++) {
-     float solution = areaofCircle(i);
-     printf("%f\n", solution);
-  }
+
+int main(int argc, char* argv[])
+{
+	if (argc == 3)
+	{
+	        int arg1;
+       		int found = sscanf(argv[1], "%d", &arg1);
+       		if (found != 1)
+       		{
+               		printf("first arg is not an integer, enter two ints\n");
+               		return 1;
+       		}
+
+	        int arg2;
+       		found = sscanf(argv[2], "%d", &arg2);
+       		if  (found != 1)
+       		{
+               		printf("second arg is not an integer, enter two ints\n");
+                	return 1;
+		}
+		for (int i = arg1; i <= arg2; i++)
+                {
+                        float ans = myAreaFunction(i);
+                        printf("%f\n", ans);
+                }
+	}
+
+	else
+	{
+		char input[256];
+		float lower;
+		float upper;
+		printf("Enter the lower bound of radii of which you want to find the area of a circle.\n");
+		while (1)
+		{
+			fgets(input, 256, stdin);
+			if (sscanf(input, "%f", &lower) == 1) break;
+			printf("Not a valid number. Try again!\n");
+		}
+		printf("Enter the upper bound of radii of which you want to find the area of a circle.\n");
+		while (1)
+		{
+			fgets(input, 256, stdin);
+			if (sscanf(input, "%f", &upper) == 1) break;
+			printf("Not a valid number. Try again!\n");
+		}
+		for (int i = lower; i <= upper; i++)
+       		{
+       	        	float ans = myAreaFunction(i);
+       	        	printf("%f\n", ans);
+       		}
+
+	}
 }
